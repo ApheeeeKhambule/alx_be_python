90e5dbf270c37865cecf5d72110b3fae25a80404
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-06-26 17:27:17 +0200

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -5,10 +5,11 @@ weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
 # Use conditional statements to provide clothing recommendations
 if weather == "sunny":
-    print("Recommendation: Wear a t-shirt and sunglasses.")
-elif weather = "rainy":
-    print("Recommendation: Don't forget your umbrella and a raincoat.")
-elif weather = "cold":
-    print("Recommendation: Make sure to wear a warm coat and a scarf.")
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
+
