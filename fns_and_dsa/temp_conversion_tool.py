317b512f9bacc2bf314e7b6cb7c08fb109d02531
ApheeeeKhambule <aphelelekhambule16@gmail.com>
2024-09-18 19:37:49 +0200

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -4,7 +4,6 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 FAHRENHEIT_OFFSET = 32
-CELSIUS_OFFSET = -32
 
 def convert_to_celsius(fahrenheit):
     """
@@ -42,4 +41,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
